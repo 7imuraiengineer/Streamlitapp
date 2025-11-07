587342e9100a6bf031b27363c092ed8d470ced92
7imur <233756151+7imuraiengineer@users.noreply.github.com>
2025-11-07 10:49:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ uploaded_file = st.file_uploader("Upload your Excel file (data.xlsx)", type=["xl
 
 if uploaded_file is not None:
 
-        def repair_xlsx(file_path):
-            try:
-                wb = openpyxl.load_workbook(file_path, data_only=True, keep_links=False)
-                new_path = file_path.replace('.xlsx', '_repaired.xlsx')
-                wb.save(new_path)
-                return new_path
-            except Exception as e:
-                print("Repair failed:", e)
-                return None
+    def repair_xlsx(file_path):
+        try:
+           wb = openpyxl.load_workbook(file_path, data_only=True, keep_links=False)
+           new_path = file_path.replace('.xlsx', '_repaired.xlsx')
+           wb.save(new_path)
+           return new_path
+         except Exception as e:
+           print("Repair failed:", e)
+           return None
 
     
    
